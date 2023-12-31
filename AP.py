1065d16145da8f60523218c3a037e880f03a318c
DGLadha <46052239+DGLadha@users.noreply.github.com>
2023-12-31 18:44:26 +0530

    Update AP.py

--- AP.py
+++ AP.py
@@ -1,4 +1,5 @@
-import streamlit as st, pandas as pd, numpy as np, plotly.express as px
+import streamlit as st, pandas as pd, numpy as np
+import plotly.express as px
 st.header('Atomic Radius Trend')
 
 
